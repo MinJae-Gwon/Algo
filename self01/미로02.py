772ceb7e6b760addba28cd661a11fb2a94ce145c
MinJae Gwon <minjae2271@gmail.com>
2019-07-09 11:06:44 +0900

    미로2

--- self01/미로02.py
+++ self01/미로02.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('미로02.txt','r')
 
 def IsSafe(x,y):
-    if x>=0 and y>=0 and x<100 and y<100:
+    if x>=0 and y>=0 and x<16 and y<16:
         return True
 
 def bfs(x,y):
@@ -16,7 +16,6 @@ def bfs(x,y):
     while Q:
         x = Q.pop(0)
         y = Q.pop(0)
-        
 
         if x == 13 and y == 13:
             canGo = True
@@ -25,25 +24,28 @@ def bfs(x,y):
         for dir in range(4):
             next_x = x + dx[dir]
             next_y = y + dy[dir]
+
             if IsSafe(next_x,next_y) and field[next_y][next_x] != 1 and visit[next_y][next_x] == 0:
+                visit[next_y][next_x] = True
                 Q.append(next_x)
                 Q.append(next_y)
 
 for time in range(10):
     case = int(input())
     field = []
-    for rows in range(100):
-        row = list(map(int,input().split()))
+    for rows in range(16):
+        row = list(map(int,list(input())))
         field.append(row)
     
     Q = []
-    visit = [[0 for _ in range(100)] for _ in range(100)]
+    visit = [[0 for _ in range(16)] for _ in range(16)]
     visit[1][1] = True
-    print(visit)
+    # print(field)
+    # print(visit)
     canGo = False
     bfs(1,1)
 
     if canGo == True:
-        print(1)
+        print('#{0} 1'.format(case))
     else:
-        print(0)
+        print('#{0} 0'.format(case))
